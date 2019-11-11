db4c4e42fd4c6da4bf4b548003ebaf5e1dabded9
Dien17 <55276872+Dien17@users.noreply.github.com>
2019-11-11 16:52:44 +0700

    my first commit

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,12 @@
-<template>   
-  <v-app>       
+<template>
+    <v-app>
       <main>
-         <router-view></router-view>       
-      </main>   
-  </v-app> 
-</template> 
- 
-<script> 
-export default {   
-  name: 'app' 
-} 
-</script> 
+        <router-view></router-view>
+      </main>
+    </v-app>
+</template>
+<script>
+export default {
+ name: 'app'
+}
+</script>
